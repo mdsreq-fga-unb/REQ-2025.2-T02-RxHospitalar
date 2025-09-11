eb253ff8d78af87086acb722877a169c88185a02
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2025-09-11 01:34:58 -0300

    fix: Atualiza o slogan na configuração do Docusaurus para refletir a marca do projeto

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'RX Hospitalar',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'BASED - RX Hospitalar',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
