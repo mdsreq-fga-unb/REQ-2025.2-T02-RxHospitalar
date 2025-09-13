ade883154ef27155284a9076dba53cad6ddcd8af
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2025-09-12 23:11:50 -0300

    feat: adiciona ata de reunião e atualiza título e descrição na lista de recursos da homepage

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -14,11 +14,11 @@ const FeatureList = [
   //   ),
   // },
   // {
-  //   title: 'Focus on What Matters',
+  //   title: 'RX Hospitalatres',
   //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
   //   description: (
   //     <>
-  //       Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+  //       Amamnda Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
   //       ahead and move your docs into the <code>docs</code> directory.
   //     </>
   //   ),
