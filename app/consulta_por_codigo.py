e5d6c4bf9df09fc485c3b1a812d254545bff5a20
daviegito <daviegitocoelho@gmail.com>
2025-11-03 20:46:10 -0300

    feat: adicionado teste da RF03

---
+++ app/consulta_por_codigo.py
@@ -0,0 +1,5 @@
+# RF03 - Consultar Produto por código original do produto
+import panda as pd
+
+def consulta_por_codigo():
+  
---
+++ tests/test_consulta_por_codigo.py
@@ -0,0 +1,45 @@
+#teste de RF03 - Consulta por código do produto
+import pandas as pd
+from app import consulta_por_codigo
+
+def test_consulta_por_codigo_com_sucesso():
+  df = pd.DataFrame([
+    {"codigo": "123", "nome": "Dipirona", "linha": "ABC"},
+    {"codigo": "456", "nome": "Dexametasona", "linha": "DEF"}
+  ])
+
+  result = consulta_por_codigo(df, "123")
+
+  assert result is not None 
+  assert result["codigo"] == "123"
+  assert result["nome"] == "Dipirona"
+  assert result["linha"] == "ABC"
+
+def test_consulta_por_codigo_nao_achada():
+  df = pd.DataFrame([
+    {"codigo": "456", "nome": "Dexametasona", "linha": "DEF"}
+  ])
+
+  result = consulta_por_codigo(df, "X987654321")
+
+  assert result is not None   
+
+def test_consulta_por_codigo_string():
+  df = pd.DataFrame([
+    {"codigo": "456", "nome": "Dexametasona", "linha": "DEF"}
+  ])
+
+  result = consulta_por_codigo(df, "AS12312")
+
+  assert result is not None
+  raise TypeError("O código é composto apenas por números inteiros")
+
+def test_consulta_por_codigo_vazio():
+  df = pd.DataFrame([
+    {"codigo": "456", "nome": "Dexametasona", "linha": "DEF"}
+  ])
+
+  result = consulta_por_codigo(df, "")
+
+  assert result is not None
+  raise ValueError("O código não pode estar vazio")  
