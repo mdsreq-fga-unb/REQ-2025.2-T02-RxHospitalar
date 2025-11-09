14bba7c8b7d34678347413a4b7ece148f9851679
daviegito <daviegitocoelho@gmail.com>
2025-11-08 21:13:42 -0300

    test: pastas de teste organizadas de melhor maneira agora

--- app/consulta_por_codigo.py
+++
@@ -1,5 +0,0 @@
-# RF03 - Consultar Produto por código original do produto
-import panda as pd
-
-def consulta_por_codigo():
-  pass
---
+++ app/models/consulta_por_linha.py
@@ -0,0 +1,6 @@
+# RF04 - Consultar produto por linha original do produto
+#import pandas as pd
+#from data_loader import file
+
+#def consulta_por_linha():
+#  pass
