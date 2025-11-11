1bc143f921b45cb836064e6658a1e593b6f58dee
Beatriz <beatrizfigueiredo008@gmail.com>
2025-11-10 21:33:35 -0300

    feat(auth): adicionar pagina de login

---
+++ app/views/pages/auth/login.py
@@ -0,0 +1,171 @@
+import tkinter as tk
+from tkinter import ttk
+from user_controller import get_email
+from utils import *
+from style import configure_styles, load_logo, create_container
+
+class LoginPage(ttk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        configure_styles()
+        
+        container = create_container(self) 
+
+        self.logo_photo = load_logo(container)
+        
+        ttk.Label(container, text="Usuário:", style="texto.TLabel").pack(pady=(45, 0), padx=(70, 0), anchor="w")
+        self.username_entry = ttk.Entry(container, width=400, style="TEntry",font=("Arial", 18))
+        self.username_entry.pack(pady=(17, 0), padx=70)
+
+        ttk.Label(container, text="Senha:", style="texto.TLabel").pack(pady=(45, 0), padx=(70, 0), anchor="w")
+        self.password_entry = ttk.Entry(container, show="•", width=400,font=("Arial", 18))
+        self.password_entry.pack(pady=(17, 0), padx=70)
+
+        self.show_password_var = tk.BooleanVar(value=False)
+        ttk.Checkbutton(
+            container,
+            text="Mostrar senha",
+            variable=self.show_password_var,
+            command=lambda: toggle_password_visibility(self.password_entry, self.show_password_var),
+            style="TCheckbutton"
+        ).pack(pady=(14, 0), padx=(0,73), anchor = "e")
+
+        ttk.Button(
+            container,
+            text="Esqueceu a senha?",
+            command=lambda: controller.show_frame("ForgotPasswordPage"),
+            style="link.TButton",
+            width=20
+        ).pack(pady=(10,0))
+
+        ttk.Button(
+            container,
+            text="Login",
+            command=lambda: validate_login(
+                self.username_entry.get(),
+                self.password_entry.get(),
+                self.feedback_label
+            ),
+            style="TButton",
+            width=400
+        ).pack(pady=(40,10), padx=70, anchor="w")
+
+        self.feedback_label = ttk.Label(container, text="",style="feedback_erro.TLabel")
+        self.feedback_label.pack()
+
+class ForgotPasswordPage(ttk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        configure_styles()
+
+        self.container = create_container(self)
+        ttk.Button(
+            self.container,
+            text="Voltar ao Login",
+            command=lambda: controller.show_frame("LoginPage"),
+            style="back.TButton"
+        ).pack()
+
+        ttk.Label(self.container, text="Recuperação de Senha", style="titulo.TLabel").pack()
+        ttk.Label(self.container, text="Digite seu email:", style="texto.TLabel").pack()
+
+        self.email_entry = ttk.Entry(self.container)
+        self.email_entry.pack()
+
+
+        self.submit_button = ttk.Button(
+            self.container,
+            text="Enviar",
+            command=self.submit_email,
+            style="TButton"
+        )
+        self.submit_button.pack()
+        self.feedback_label = ttk.Label(self.container, text="",style="feedback_erro.TLabel")
+        self.feedback_label.pack(pady=5)
+
+        
+
+    def submit_email(self):
+        email = self.email_entry.get()
+        if get_email() == email:
+            self.feedback_label.config(style="feedback_ok.TLabel", text="Email verificado! Digite a nova senha:")
+            self.submit_button.config(state="disabled")
+
+            ttk.Label(self.container, text="Nova Senha:", style="texto.TLabel").pack()
+            self.password_entry = ttk.Entry(self.container, show="•")
+            self.password_entry.pack()
+
+            self.show_password_var = tk.BooleanVar(value=False)
+            ttk.Checkbutton(
+                self.container,
+                text="Mostrar senha",
+                variable=self.show_password_var,
+                command=lambda: toggle_password_visibility(self.password_entry, self.show_password_var),
+                style="TCheckbutton"
+            ).pack()
+
+            ttk.Button(
+                self.container,
+                text="Confirmar",
+                command=lambda: update_password(
+                    self.password_entry.get(),
+                    self.feedback_label,
+                    self.controller
+                ),
+                style="TButton"
+            ).pack()
+        else:
+            self.feedback_label.config(style="feedback_erro.TLabel", text="Email não encontrado")
+
+
+
+
+
+
+
+
+#ir para run.py
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Sistema RX Hospitalar")
+        self.geometry("800x800") 
+        self.bg="#000000"
+        # Guarda referência do frame atual
+        self.current_frame = None
+
+        # Mostra a primeira página
+        self.show_frame("LoginPage")
+
+    def show_frame(self, page_name):
+        """Cria a página do zero e exibe."""
+        # Destroi o frame atual (se existir)
+        if self.current_frame is not None:
+            self.current_frame.destroy()
+
+        # Mapeia o nome da página para a classe correspondente
+        pages = {
+            "LoginPage": LoginPage,
+            "ForgotPasswordPage": ForgotPasswordPage
+        }
+
+        page_class = pages.get(page_name)
+        if not page_class:
+            print(f"[ERRO] Página '{page_name}' não encontrada.")
+            return
+
+        # Cria uma nova instância e mostra
+        new_frame = page_class(self, self)
+        new_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+        # Atualiza a referência
+        self.current_frame = new_frame
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
---
+++ app/views/pages/auth/utils.py
@@ -0,0 +1,28 @@
+#funções auxiliares login
+
+from user_controller import get_user, change_password
+
+def toggle_password_visibility(password_entry, show_password_var):
+    if show_password_var.get():
+        password_entry.config(show="")
+    else:
+        password_entry.config(show="•")
+
+def validate_login(username, password, feedback_label):
+    if get_user(username, password):
+        feedback_label.config(style="feedback_ok.TLabel", text="Login bem-sucedido!")
+        return True
+    else:
+        feedback_label.config(style="feedback_erro.TLabel",text="Usuário ou senha incorretos.")
+        return False
+
+
+def update_password(password_entry, feedback_label, controller):
+    new_password = password_entry
+    if change_password(new_password):                          #futuramente validação da senha
+        feedback_label.config(style="feedback_ok.TLabel", text="Senha alterada com sucesso!")
+        feedback_label.after(1000, lambda: controller.show_frame("LoginPage"))
+        return True
+    else:
+        feedback_label.config(style="feedback_erro.TLabel", text="Erro ao alterar a senha.")
+        return False
