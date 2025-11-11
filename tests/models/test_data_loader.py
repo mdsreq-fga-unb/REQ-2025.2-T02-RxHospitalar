24da4343a7abdeb1c1e8acd2c99d8d8b965fd7f0
daviegito <daviegitocoelho@gmail.com>
2025-11-11 19:36:23 -0300

    test: adição de 1 teste a mais com outra aba. Está funcional

--- tests/models/test_data_loader.py
+++ tests/models/test_data_loader.py
@@ -1,7 +1,23 @@
 import pandas as pd
 from app.models.data_loader import load_selected_columns
 
-def test_load_selected_columns():
+def test_load_selected_columns_vendas():
+    # definindo a planilha e as colunas a serem carregadas
+    sheet = "Vendas_Pendencia"
+    cols = ["RAZAOSOCIAL", "OBS"]
+    
+    # carregando os dados usando a função do data_loader
+    df = load_selected_columns(sheet, cols)
+    
+    # verificações simples 
+    assert isinstance(df, pd.DataFrame), "A função deve retornar um DataFrame"
+    
+    # verificando se as colunas carregadas são as esperadas
+    assert not df.empty, "O DataFrame não deveria estar vazio para essa planilha"
+   
+    assert list(df.columns) == cols, "As colunas retornadas devem ser as mesmas solicitadas"
+    
+def test_load_selected_columns_estoque():
     # definindo a planilha e as colunas a serem carregadas
     sheet = "Estoque"
     cols = ["Preço Aquisição", "Sit. Fiscal"]
@@ -16,4 +32,3 @@ def test_load_selected_columns():
     assert not df.empty, "O DataFrame não deveria estar vazio para essa planilha"
    
     assert list(df.columns) == cols, "As colunas retornadas devem ser as mesmas solicitadas"
-    
