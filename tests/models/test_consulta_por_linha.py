318417da6d544e71185091f46aa0a93b3f95af13
daviegito <daviegitocoelho@gmail.com>
2025-11-16 18:50:57 -0300

    test: testes devidamente comentados

---
+++ tests/models/test_consulta_por_linha.py
@@ -0,0 +1,47 @@
+#teste de RF04 - Consulta por linha do produto
+import pandas as pd
+import pytest
+from app.models.consulta_por_linha import consulta_por_linha
+from app.models.data_loader import file
+
+# teste para escolha da coluna na qual haverá a busca
+def _escolher_coluna_codigo(cols):
+  cand = ["Grupo", "Sub Grupo Nível 1"]
+  for c in cand:
+    if c in cols:
+      return c
+  # para procurar colunas relacionadas
+  for c in cols:
+    if str(c).lower().startswith(("gru", "GRU", "Gru")):
+      return c
+  return None
+
+# teste do caso de sucesso para a busca de um grupo
+def test_consulta_por_linha_com_sucesso_grupo():
+  df0 = pd.read_excel(file, sheet_name=0, header=0, dtype=str)
+  col = _escolher_coluna_codigo(df0.columns)
+  assert col is not None, "Nenhuma coluna da linha encontrada"
+  linha_produto = "BBRAUN"
+  result = consulta_por_linha(linha_produto)
+  assert result is not None
+  assert result["Grupo"] == linha_produto
+
+# teste do caso de sucesso para a busca de um subgrupo
+def test_consulta_por_linha_com_sucesso_sub_grupo():
+  df0 = pd.read_excel(file, sheet_name=0, header=0, dtype=str)
+  col = _escolher_coluna_codigo(df0.columns)
+  assert col is not None, "Nenhuma coluna da linha encontrada"
+  linha_produto = "IV ACESS"
+  result = consulta_por_linha(linha_produto)
+  assert result is not None
+  assert result["Sub Grupo Nível 1"] == linha_produto
+
+# teste para linha não encontrada/cadastrada
+def test_consulta_por_linha_nao_achada():
+  with pytest.raises(ValueError, match="A linha não foi encontrada"):
+    consulta_por_linha("INEXISTENTE")
+
+# teste para consulta vazia de linha
+def test_consulta_por_linha_vazio():
+  with pytest.raises(ValueError, match="A linha não foi encontrada"):
+    consulta_por_linha("")
--- tests/models/test_data_loader.py
+++ tests/models/test_data_loader.py
@@ -2,33 +2,33 @@ import pandas as pd
 from app.models.data_loader import load_selected_columns
 
 def test_load_selected_columns_vendas():
-    # definindo a planilha e as colunas a serem carregadas
+    #definindo a planilha e as colunas a serem carregadas
     sheet = "Vendas_Pendencia"
     cols = ["RAZAOSOCIAL", "OBS"]
     
-    # carregando os dados usando a função do data_loader
+    #carregando os dados usando a função do data_loader
     df = load_selected_columns(sheet, cols)
     
-    # verificações simples 
+    #verificações simples 
     assert isinstance(df, pd.DataFrame), "A função deve retornar um DataFrame"
     
-    # verificando se as colunas carregadas são as esperadas
+    #verificando se as colunas carregadas são as esperadas
     assert not df.empty, "O DataFrame não deveria estar vazio para essa planilha"
    
     assert list(df.columns) == cols, "As colunas retornadas devem ser as mesmas solicitadas"
     
 def test_load_selected_columns_estoque():
-    # definindo a planilha e as colunas a serem carregadas
+    #definindo a planilha e as colunas a serem carregadas
     sheet = "Estoque"
     cols = ["Preço Aquisição", "Sit. Fiscal"]
     
-    # carregando os dados usando a função do data_loader
+    #carregando os dados usando a função do data_loader
     df = load_selected_columns(sheet, cols)
     
-    # verificações simples 
+    #verificações simples 
     assert isinstance(df, pd.DataFrame), "A função deve retornar um DataFrame"
     
-    # verificando se as colunas carregadas são as esperadas
+    #verificando se as colunas carregadas são as esperadas
     assert not df.empty, "O DataFrame não deveria estar vazio para essa planilha"
    
     assert list(df.columns) == cols, "As colunas retornadas devem ser as mesmas solicitadas"
